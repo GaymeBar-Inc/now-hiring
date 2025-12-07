e3121e29fd5b116511b86606f5401a80762b8871
jasontoups <me@jasontoups.com>
2025-12-07 12:29:22 -0800

    medium impact update

--- src/heros/MediumImpact/index.tsx
+++ src/heros/MediumImpact/index.tsx
@@ -9,11 +9,11 @@ import RichText from '@/components/RichText'
 export const MediumImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   return (
     <div className="hero-medium-impact flex flex-row items-center justify-center">
-      <div className="container mb-8">
-        {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
+      <div className="content-container mb-8 mr-4">
+        {richText && <RichText className="mb-6" data={richText} enableGutter={true} />}
 
         {Array.isArray(links) && links.length > 0 && (
-          <ul className="flex gap-4">
+          <ul className="flex container gap-4">
             {links.map(({ link }, i) => {
               return (
                 <li key={i}>
@@ -24,15 +24,10 @@ export const MediumImpactHero: React.FC<Page['hero']> = ({ links, media, richTex
           </ul>
         )}
       </div>
-      <div className="container ">
+      <div className="media-container">
         {media && typeof media === 'object' && (
           <div>
-            <Media
-              className="-mx-4 md:-mx-8 2xl:-mx-16"
-              imgClassName=""
-              priority
-              resource={media}
-            />
+            <Media className="" imgClassName="" priority resource={media} />
             {media?.caption && (
               <div className="mt-3">
                 <RichText data={media.caption} enableGutter={false} />
